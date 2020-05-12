fbddf8922f33ba522541dbbc76c335627eb9d808
jczhangwei <jczhangwei@foxmail.com>
2020-05-13 03:22:53 +0800

    new

--- 16.最接近的三数之和.py
+++ 16.最接近的三数之和.py
@@ -26,5 +26,30 @@
 # @lc code=start
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        l = len(nums)
+        if l < 3:
+            return
+        res = sys.maxsize
+        for k, n in enumerate(nums):
+            if k > 0 and n == nums[k - 1]:
+                continue
+            i = k + 1
+            j = l - 1
+            while i < j:
+                a = n + nums[i] + nums[j]
+                if abs(target - a) < abs(target - res):
+                    res = a
+                if a > target and i < j:
+                    j = j - 1
+                    while nums[j] == nums[j + 1] and i < j:
+                        j = j - 1
+                if a < target and i < j:
+                    i = i + 1
+                    while nums[i] == nums[i - 1] and i < j:
+                        i = i + 1
+                if a == target:
+                    return a
+        return res
 # @lc code=end
 
