93cfb46c40ccdfedca3365d25c265d1caffba45f
jczhangwei <jczhangwei@foxmail.com>
2020-05-15 02:36:54 +0800

    new

--- 141.环形链表.py
+++ 141.环形链表.py
@@ -71,12 +71,12 @@ class Solution:
         slow = head
         fast = head
         while fast and fast.next:
-            if fast == slow:
-                return True
-            
             fast = fast.next.next
             slow = slow.next
-        
+
+            if fast == slow:
+                return True
+
         return False
             
 # @lc code=end
