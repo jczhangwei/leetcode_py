6dab5806bf150dcafea4ce6eda29ba9da1221eeb
jczhangwei <jczhangwei@foxmail.com>
2020-06-29 00:27:33 +0800

    new

--- 416.分割等和子集.py
+++ 416.分割等和子集.py
@@ -14,40 +14,41 @@
 # Testcase Example:  '[1,5,11,5]'
 #
 # 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
-# 
+#
 # 注意:
-# 
-# 
+#
+#
 # 每个数组中的元素不会超过 100
 # 数组的大小不会超过 200
-# 
-# 
-# 示例 1:
-# 
-# 输入: [1, 5, 11, 5]
-# 
-# 输出: true
-# 
-# 解释: 数组可以分割成 [1, 5, 5] 和 [11].
-# 
-# 
-# 
-# 
-# 示例 2:
-# 
-# 输入: [1, 2, 3, 5]
-# 
-# 输出: false
-# 
-# 解释: 数组不能分割成两个元素和相等的子集.
-# 
-# 
-# 
-# 
 #
+#
+# 示例 1:
+#
+# 输入: [1, 5, 11, 5]
+#
+# 输出: true
+#
+# 解释: 数组可以分割成 [1, 5, 5] 和 [11].
+#
+#
+#
+#
+# 示例 2:
+#
+# 输入: [1, 2, 3, 5]
+#
+# 输出: false
+#
+# 解释: 数组不能分割成两个元素和相等的子集.
+#
+#
+#
+#
+#
+from typing import *
 
 # @lc code=start
-from typing import *
+
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
@@ -69,13 +70,11 @@ class Solution:
                     dp[i][j] = dp[i - 1][j] or dp[i-1][j - nums[i]]
 
         return dp[l - 1][t]
-            
-        
+
+# @lc code=end
+
 
 assert Solution().canPartition([]) == False
 assert Solution().canPartition([0]) == True
 assert Solution().canPartition([1, 5, 11, 5]) == True
 assert Solution().canPartition([1, 2, 3, 5]) == False
-        
-# @lc code=end
-
