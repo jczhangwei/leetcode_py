74865c14634e74fda730bbd21cfbb4d16775c251
jczhangwei <jczhangwei@foxmail.com>
2020-07-02 00:52:39 +0800

    new

--- 416.分割等和子集.py
+++ 416.分割等和子集.py
@@ -111,10 +111,10 @@ class Solution:
 # @lc code=end
 
 
-# assert Solution().canPartition([]) == False
-# assert Solution().canPartition([0]) == True
-# assert Solution().canPartition([1, 5, 11, 5]) == True
-# assert Solution().canPartition([1, 2, 3, 5]) == False
+assert Solution().canPartition([]) == False
+assert Solution().canPartition([0]) == True
+assert Solution().canPartition([1, 5, 11, 5]) == True
+assert Solution().canPartition([1, 2, 3, 5]) == False
 
 assert Solution().canPartition2([]) == False
 assert Solution().canPartition2([0]) == True
