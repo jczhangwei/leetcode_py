168867d5967330fa3aff44af4fc3ac581df44fb5
jczhangwei <jczhangwei@foxmail.com>
2020-10-18 16:23:47 +0800

    new

--- 311.稀疏矩阵的乘法.py
+++ 311.稀疏矩阵的乘法.py
@@ -9,8 +9,6 @@
 
 # 请仔细阅读下面的示例。
 
- 
-
 # 示例：
 
 # 输入：
@@ -33,10 +31,54 @@
 #                   | 0 0 1 |
 # 通过次数1,861提交次数2,440
 
+from collections import defaultdict
 from typing import *
+import collections
 # @lc code=start
+
+
 class Solution:
-    def isOneEditDistance(self, s: str, t: str) -> bool:
-        pass
+    def multiply(self, A: List[List[int]],
+                 B: List[List[int]]) -> List[List[int]]:
+        indexA = {}
+        for i in range(len(A)):
+            indexA[i] = {}
+            for j in range(len(A[0])):
+                if A[i][j]:
+                    indexA[i][j] = A[i][j]
+
+        indexB = {}
+        for j in range(len(B[0])):
+            indexB[j] = {}
+            for i in range(len(B)):
+                if B[i][j]:
+                    indexB[j][i] = B[i][j]
+
+        res = [[0 for i in range(len(B[0]))] for i in range(len(A))]
+        for i in range(len(indexA)):
+            for j in range(len(indexB)):
+                for k, v in indexA[i].items():
+                    if indexB[j].get(k):
+                        res[i][j] = res[i][j] + indexA[i][k] * indexB[j][k]
+
+        return res
+
+
+class Solution2:
+    def multiply(self, A: List[List[int]],
+                 B: List[List[int]]) -> List[List[int]]:
+        res = [[0 for i in range(len(B[0]))] for i in range(len(A))]
+        for i in range(len(res)):
+            for j in range(len(res[0])):
+                for k in range(len(A[0])):
+                    res[i][j] = res[i][j] + A[i][k] * B[k][j]
+
+        return res
+
+
 # @lc code=end
 
+
+print(Solution().multiply([[1, 0, 0], [-1, 0, 3]],
+                          [[7, 0, 0], [0, 0, 0], [0, 0, 1]]))
+print(Solution().multiply([[1, -5]], [[12], [-1]]))
