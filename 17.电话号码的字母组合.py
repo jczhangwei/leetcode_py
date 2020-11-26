acc6a74224713d03e950bb88022a69bba807fe1b
zhangwei <jczhangwei@foxmail.com>
2020-11-27 00:07:34 +0800

    new

---
+++ 17.电话号码的字母组合.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode.cn id=17 lang=python3
+#
+# [17] 电话号码的字母组合
+#
+# https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/description/
+#
+# algorithms
+# Medium (55.52%)
+# Likes:    1014
+# Dislikes: 0
+# Total Accepted:    196.6K
+# Total Submissions: 353.9K
+# Testcase Example:  '"23"'
+#
+# 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+#
+# 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+#
+#
+#
+# 示例:
+#
+# 输入："23"
+# 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+#
+#
+# 说明:
+# 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+#
+#
+from typing import *
+# @lc code=start
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+        res = []
+        def back(cur, digs):
+            if len(digs) <= 0:
+                res.append(cur)
+            else:
+                for _, p in enumerate(phone[digs[0]]):
+                    back(cur + p, digs[1:])
+        back("", digits)
+        return res
+        
+# @lc code=end
+
+print(Solution().letterCombinations("23"))
